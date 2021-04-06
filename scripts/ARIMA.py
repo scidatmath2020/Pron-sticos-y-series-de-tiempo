7b4c1c19f6244941d6498715927711897f68c744
scidatmath2020 <56654363+scidatmath2020@users.noreply.github.com>
2021-04-06 17:34:49 -0500

    Add files via upload

--- scripts/ARIMA.py
+++ scripts/ARIMA.py
@@ -33,7 +33,6 @@ del delitos["Robo_pv"]
 delitos
 
 delitos["media_movil"] = delitos.rolling(window=12).mean()
-
 delitos.plot()
 
 #%%
@@ -48,13 +47,21 @@ delitos.plot()
 
 sts.adfuller(delitos["totales"])  # Se observa que no es estacionaria
 
-sgt.plot_pacf(delitos["totales"].diff().dropna(), zero = False, lags=40,method="ols")
+sgt.plot_acf(delitos["totales"].diff().dropna(), zero = False, lags=40)
+sgt.plot_acf(delitos["totales"].diff().dropna().diff().dropna(), zero = False, lags=40)
 
 sts.adfuller(delitos["totales"].diff().dropna())
 sts.adfuller(delitos["totales"].diff().dropna().diff().dropna())
 
 delitos["totales"].diff().dropna().std()
 delitos["totales"].diff().dropna().diff().dropna().std()
+
+delitos["totales"].plot()
+delitos["totales"].diff().dropna().plot()
+delitos["totales"].diff().dropna().diff().dropna().plot()
+
+d=1
+'''Concluímos que d=1'''
 
 #%%
 
@@ -74,7 +81,7 @@ q=12
 
 #%%
 
-modelo = ARIMA(delitos["totales"],order=(p,1,q))
+modelo = ARIMA(delitos["totales"],order=(p,d,q))
 resultados = modelo.fit()
 plt.plot(delitos["totales"])
 plt.plot(resultados.fittedvalues,color="red")
@@ -85,12 +92,20 @@ print(resultados.summary())
 #%%
 
 '''Análisis de los residuos'''
+
+resultados.resid
+
 residuals = pd.DataFrame(resultados.resid)
 fig, ax = plt.subplots(1,2)
 residuals.plot(title="Residuals", ax=ax[0])
 residuals.plot(kind='kde', title='Density', ax=ax[1])
 
+#%%
 
+resultados.predict(0,108).plot()
+delitos["totales"].plot()
+
+#%%
 
 
 
